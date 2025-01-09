f662051f5a439697c4558009e8154d73dafb1c97
sangsang000000 <hsm2102@naver.com>
2025-01-09 19:58:46 +0900

    chore: 기존과 달리 게시글 상세 페이지 정보를 요청하면 해당 게시글에 적힌 댓글 정보도 같이 응답하도록 수정

--- articles/serializers.py
+++ articles/serializers.py
@@ -3,52 +3,6 @@ from rest_framework import serializers
 from .models import Article
 from .models import Comment
 
-
-class ArticleListSerializer(serializers.ModelSerializer):
-    """게시글 목록 조회 Serializer"""
-    class Meta:
-        model = Article
-        fields = (
-            "id",
-            "author",
-            "title",
-            "created_at",
-            "image",
-            "rating"
-        )
-        read_only_fields = ("author",)
-
-
-class ArticleDetailSerializer(serializers.ModelSerializer):
-    """게시글 상세 조회 및 수정 및 생성 Serializer"""
-
-    author = serializers.ReadOnlyField(
-        source="author.email"
-    )  # author 필드에 작성자의 이메일만 출력
-    image = serializers.ImageField()
-
-    class Meta:
-        model = Article
-        fields = (
-            "id",
-            "author",
-            "Article_title",
-            "movie_title",
-            "content",
-            "image",
-            "created_at",
-            "updated_at",
-            "rating",
-        )
-
-    def get_image(self, obj):
-        request = self.context.get("request")  # Serializer context에서 request 가져오기
-        if obj.image:
-            return request.build_absolute_uri(obj.image.url)
-        return None
-    
-
-
 class CommentSerializer(serializers.ModelSerializer):
     """댓글 조회 및 생성 및 수정 Serializer"""
 
@@ -67,3 +21,53 @@ class CommentSerializer(serializers.ModelSerializer):
             
         )
         read_only_fields = ("article", "like_users")
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    """게시글 목록 조회 Serializer"""
+    class Meta:
+        model = Article
+        fields = (
+            "id",
+            "author",
+            "Article_title",
+            "movie_title",
+            "created_at",
+            "image",
+            "rating"
+        )
+        read_only_fields = ("author",)
+
+
+class ArticleDetailSerializer(serializers.ModelSerializer):
+    """게시글 상세 조회 및 수정 및 생성 Serializer"""
+    comments = CommentSerializer(many=True, read_only=True)
+
+    author = serializers.ReadOnlyField(
+        source="author.email"
+    )  # author 필드에 작성자의 이메일만 출력
+    image = serializers.ImageField()
+
+    class Meta:
+        model = Article
+        fields = (
+            "id",
+            "author",
+            "Article_title",
+            "movie_title",
+            "content",
+            "image",
+            "created_at",
+            "updated_at",
+            "rating",
+            "comments",
+        )
+
+    def get_image(self, obj):
+        request = self.context.get("request")  # Serializer context에서 request 가져오기
+        if obj.image:
+            return request.build_absolute_uri(obj.image.url)
+        return None
+    
+
+
+
