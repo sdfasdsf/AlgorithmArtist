6ffe80383785cd3c9b6eaefc456c50c8bb8e0c6f
sangsang000000 <hsm2102@naver.com>
2025-01-25 04:01:28 +0900

    feat: 메인 페이지 수정 및 영화 상세 페이지 생성

--- Main/urls.py
+++ Main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import Main, movie, movieboard, reviewboard
+
+
 
 urlpatterns = [
-    path('', Main, name="Main"),
-    path('movie/', movie, name="movie"),
-    path('movieboard/', movieboard, name="movieboard"),
-    path('reviewboard/', reviewboard, name="reviewboard"),
+    path('', views.Main.as_view(), name="Main"),
+    path('movie/<int:movie_id>/', views.Movie.as_view(), name="movie"),
+
 ]
